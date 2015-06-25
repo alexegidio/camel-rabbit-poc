6d926e2e4b671aa90c5c0738ca54bddb2466ab28
peo_aegidio <alex@localhost.localdomain>
2015-06-25 16:26:01 -0300

    Removes string com routeName

--- src/main/java/br/com/camel/poc/infra/camel/routes/TestRoute.java
+++ src/main/java/br/com/camel/poc/infra/camel/routes/TestRoute.java
@@ -1,6 +1,10 @@
 package br.com.camel.poc.infra.camel.routes;
 
+import br.com.camel.poc.business.StringMessageProcessor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.spring.SpringRouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * @project: camel-rabbit-poc
@@ -10,11 +14,12 @@ import org.apache.camel.builder.RouteBuilder;
  */
 public class TestRoute extends RouteBuilder {
 
+
     @Override
     public void configure() throws Exception {
 
         from("rabbitmq://localhost:5672/camel-poc-exchange?exchangeType=fanout&autoDelete=false")
                 .unmarshal().string("UTF-8")
-                .beanRef("stringMessageProcessor", "process");
+                .bean(StringMessageProcessor.class);
     }
 }
